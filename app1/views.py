ada31ec66cfa174c9db6b7b6bd140f55f1fcc25b
koushikromel <koushikromel@gmail.com>
2023-04-18 16:30:18 +0530

    added koushik and a dynamic url

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def koushik(request):
+    return HttpResponse("Hello Koushik")
+
+def ananymous(request, name):
+    return HttpResponse("Hello " + name + " welcome to our website")
